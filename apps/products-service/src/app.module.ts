a290866a3a9ac6b8d4181898d6ed23a1453a5cd7
Oleg Plema <olegplema@gmail.com>
2026-06-13 13:33:16 +0300

    refactor(products): introduce module structure

--- apps/products-service/src/app.module.ts
+++ apps/products-service/src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ProductsModule } from './modules/products/products.module';
 
 @Module({
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ProductsModule],
 })
 export class AppModule {}
--- apps/products-service/src/app.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class AppService {
-  getStatus(): string {
-    return 'Products service is running';
-  }
-}
